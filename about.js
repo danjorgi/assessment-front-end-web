fa1a1c5e0695e2aa80b23396abb7d681167332eb
Daniel Jorgensen <dan.r.jorgensen@gmail.com>
2023-10-10 15:48:19 -0600

    Part 2 complete

--- about.js
+++ about.js
@@ -5,10 +5,18 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Your information has been submitted!')
 }
 
 
 let form = document.querySelector('#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+let duckCompliment = () => {
+	alert(`You're as cute as a duck!`)
+}
+
+let ducky = document.querySelector('img');
+
+ducky.addEventListener('mouseover', duckCompliment);
